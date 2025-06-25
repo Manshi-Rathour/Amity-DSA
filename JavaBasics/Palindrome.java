99700da4dad3f07ed6230b56563bcd1193d7e6f8
Manshi Rathour <manshi2086@gmail.com>
2025-06-25 22:11:37 +0530

    palindrome or not

---
+++ JavaBasics/Palindrome.java
@@ -0,0 +1,29 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number to check whether it is Palindrome or not: ");
+        int num = sc.nextInt();
+
+        int temp = num;
+        int reverseNum = 0;
+        while(temp != 0){
+            int ld = temp%10;
+            reverseNum = reverseNum * 10 + ld;
+            temp = temp/10;
+        }
+
+        if(num == reverseNum){
+            System.out.println("Number is Palindrome");
+            System.out.println(reverseNum);
+        }
+        else{
+            System.out.println("Number is not Palindrome");
+        }
+
+    }
+}
