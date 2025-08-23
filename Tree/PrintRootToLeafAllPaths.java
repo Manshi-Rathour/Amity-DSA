68407c7d8d1f025e68947a0f0d9a8b0102cb0da4
Manshi Rathour <manshi2086@gmail.com>
2025-08-23 20:58:12 +0530

    bst ques

---
+++ Tree/PrintRootToLeafAllPaths.java
@@ -0,0 +1,65 @@
+package Tree;
+
+import java.util.ArrayList;
+
+public class PrintRootToLeafAllPaths {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data > val){
+            // left subtree
+            root.left = insert(root.left, val);
+        }
+        else{
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for(int i=0; i<path.size(); i++){
+            System.out.print(path.get(i) + " -> ");
+        }
+        System.out.println("Null");
+    }
+    public static void printRootToLeafAllPaths(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+        path.add(root.data);
+
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+
+        printRootToLeafAllPaths(root.left, path);
+        printRootToLeafAllPaths(root.right, path);
+
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] values = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        Node root = null;
+
+        for(int i=0; i<values.length; i++){
+            root = insert(root, values[i]);
+        }
+
+        printRootToLeafAllPaths(root, new ArrayList<>());
+    }
+}
