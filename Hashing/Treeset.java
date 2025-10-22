09cf12906a25c7c687255e3529f780519a186854
Manshi Rathour <manshi2086@gmail.com>
2025-10-22 10:45:22 +0530

    hashing questions

---
+++ Hashing/Treeset.java
@@ -0,0 +1,14 @@
+package Hashing;
+
+import java.util.TreeSet;
+
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("Mumbai");
+        ts.add("Banglore");
+        ts.add("Delhi");
+
+        System.out.println(ts);
+    }
+}
