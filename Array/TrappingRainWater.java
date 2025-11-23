17f0a185b86814932731d1143492d6df24d32597
Manshi Rathour <manshi2086@gmail.com>
2025-11-23 10:16:53 +0530

    trapping rain water updated

--- Array/TrappingRainWater.java
+++ Array/TrappingRainWater.java
@@ -29,6 +29,6 @@ public class TrappingRainWater {
     public static void main(String[] args) {
         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
 
-        System.out.println(trap(height));
+        System.out.println("Total trapped water: " + trap(height));
     }
 }
